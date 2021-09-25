44210ca7c1d56b9f17d327777c05d2e4696daa9c
astranabawi <1902658@sit.singaporetech.edu.sg>
2021-09-25 23:15:28 +0800

    update find location by mac address

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+# Functional Requirements:
+# 1. The system shall be able to detect the location of the user (response time)
+# 2. The system shall be able to detect the number of users at a particular location at a particular time (throughput)
+# 3. The system shall be able to get alerts of the location which is unpatrolled for a certain period. (response time)
+import time
+
 from flask import Flask, render_template
 from flask import request
 from flask_ngrok import run_with_ngrok
@@ -28,41 +34,54 @@ def get_beacon_info():
         print("start time: " + str(start_time))
         print("end time: " + str(end_time))
         print("staff id: " + str(staff_id))
-        payload_dict = {
-            "location": [
-                {
-                    "level": 3,
-                    "location": "ROOM 4",
-                    "timestamp": start_time + 1
-                },
-                {
-                    "level": 7,
-                    "location": "SR7D",
-                    "timestamp": start_time + 2
-                },
-                {
-                    "level": 7,
-                    "location": "ROOM 4",
-                    "timestamp": start_time + 3
-                }
-            ]
-        }
-        return payload_dict
+        # payload_dict = {
+        #     "location": [
+        #         {
+        #             "level": 3,
+        #             "location": "ROOM 4",
+        #             "timestamp": start_time + 1
+        #         },
+        #         {
+        #             "level": 7,
+        #             "location": "SR7D",
+        #             "timestamp": start_time + 2
+        #         },
+        #         {
+        #             "level": 7,
+        #             "location": "ROOM 4",
+        #             "timestamp": start_time + 3
+        #         }
+        #     ]
+        # }
 
 
 # retrieve beacon information from android phone (staff id, rssi and mac address)
 @app.route("/beaconinfo", methods=["POST"])
 def beaconinfo():
+    timestamp = int(time.time())
     staff_id = request.form["staffId"]
     rssiInput = request.form["rssiInput"]
     macInput = request.form["macInput"]
-    addNewRecord(staff_id, rssiInput, macInput)
-    print(beaconLocList)
+    addNewRecord(staff_id, macInput, rssiInput, timestamp)
+    print(beaconLocDict)
 
 
 # add record into beacon location list
-def addNewRecord(staff_id, mac, rssi):
-    beaconLocList.append({'staff_id': staff_id, 'mac': mac, 'rssi': rssi})
+def addNewRecord(staff_id, mac, rssi, timestamp):
+    location = findLocationByMac(mac)
+    if staff_id in beaconLocDict:
+        beaconLocDict[staff_id].append({'rssi': rssi, 'location': location, 'timestamp': timestamp })
+    else:
+        beaconLocDict[staff_id] = [{'rssi': rssi, 'location': location, 'timestamp': timestamp}]
+
+
+# find location based on mac address:
+def findLocationByMac(mac):
+    for i, row in df.iterrows():
+        if row['mac'] == mac:
+            return row['location']
+        else:
+            return None
 
 
 # import location based on mac address from text file
@@ -76,6 +95,6 @@ def readBeaconLocations():
 
 if __name__ == "__main__":
     df = readBeaconLocations()
-    beaconLocList = []  # store latest beacon updates from android
+    beaconLocDict = {}  # store latest beacon updates from android
     # beaconLocList.append({'staff_id': 1, 'mac': 'abcde1234', 'rssi': -55})
     app.run()
