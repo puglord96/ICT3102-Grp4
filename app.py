f808af044a8574881f3345c36e260a6a1c2d131b
astranabawi <1902658@sit.singaporetech.edu.sg>
2021-09-30 22:15:08 +0800

    threaded

--- app.py
+++ app.py
@@ -169,4 +169,4 @@ if __name__ == "__main__" or __name__ == "app":
     sched_1.start()
     ##################################################
     if __name__ == "__main__":
-        app.run(host='0.0.0.0', port=5000)
+        app.run(threaded=True, host='0.0.0.0', port=5000)
